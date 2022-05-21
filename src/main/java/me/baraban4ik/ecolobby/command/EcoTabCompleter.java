df568688c45ad90716e4f9e24d4b1dcccecbc259
Baraban4ik <vovapetrenko890@gmail.com>
2022-05-22 02:50:07 +0600

    Bump version

---
+++ src/main/java/me/baraban4ik/ecolobby/command/EcoTabCompleter.java
@@ -0,0 +1,32 @@
+package me.baraban4ik.ecolobby.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class EcoTabCompleter implements TabCompleter {
+
+    public List<String> complete(CommandSender sender, String[] args) {
+        return null;
+    }
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
+        return filter(complete(sender, args), args);
+    }
+
+    private  List<String> filter(List<String> list, String[] args) {
+        if(list == null) return null;
+        String last = args[args.length - 1];
+        List<String> result = new ArrayList<>();
+        for (String arg : list) {
+            if (arg.toLowerCase().startsWith(last.toLowerCase())) result.add(arg);
+
+        }
+        return result;
+    }
+}
